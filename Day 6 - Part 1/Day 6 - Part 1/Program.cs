4812ce0924abbf4b6bb166d6d5e28798cc40f1d5
Daniel Sharp <148597082+LuckyPepsiBest@users.noreply.github.com>
2023-12-06 17:57:34 +0000

    Add files via upload

---
+++ Day 6 - Part 1/Day 6 - Part 1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Mime;
+using System.Windows.Markup;
+
+namespace Day_6___Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> time = new List<int>();
+            List<int> distance = new List<int>();
+            string line = "";
+            string line2 = "";
+            using(StreamReader sr = new StreamReader("txt.txt"))
+            {
+                while(!sr.EndOfStream)
+                {
+                    
+                    line = sr.ReadLine();
+                    int colon = line.IndexOf(':');
+                    line2 = line.Substring(0, colon);
+                    colon++;
+                    line = line.Substring(colon).Trim();
+                    string[] vals = line.Split(' ');
+                    if (line2 == "Time")
+                    {
+                        for (int i = 0; i < vals.Length; i++)
+                        {
+                            if (vals[i] == "") ;
+                            else
+                            {
+                                time.Add(int.Parse(vals[i].Trim()));
+                            }
+                        }
+                    }
+                    else if (line2 == "Distance")
+                    {
+                        for (int i = 0; i < vals.Length; i++)
+                        {
+                            if (vals[i] == "") ;
+                            else
+                            {
+                                distance.Add(int.Parse(vals[i].Trim()));
+                            }
+                        }
+                    }
+                }
+            }
+            int total = 0;
+            int next = 1;
+            for (int i = 0; i < time.Count; i++)
+            {
+                total = 0;
+                for (int j = 0; j < time[i]; j++)
+                {
+                    if (j * (time[i] - j) > distance[i])
+                    {
+                        total++;
+                    }
+                }
+                next = total * next;
+            }
+
+            Console.WriteLine(next);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 6 - Part 2/Day 6 - Part 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Mime;
+using System.Windows.Markup;
+using System.Security.Cryptography;
+
+namespace Day_6___Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> time = new List<long>();
+            List<long> distance = new List<long>();
+            string line = "";
+            string line2 = "";
+            string time2 = "";
+            string distance2 = "";
+            using (StreamReader sr = new StreamReader("txt.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                    line = sr.ReadLine();
+                    int colon = line.IndexOf(':');
+                    line2 = line.Substring(0, colon);
+                    colon++;
+                    line = line.Substring(colon).Trim();
+                    string[] vals = line.Split(' ');
+                    if (line2 == "Time")
+                    {
+                        for (int i = 0; i < vals.Length; i++)
+                        {
+                            if (vals[i] == "") ;
+                            else
+                            {
+                                time2 = time2 + vals[i];
+                            }
+                        }
+                        time.Add(long.Parse(time2));
+                    }
+                    else if (line2 == "Distance")
+                    {
+                        for (int i = 0; i < vals.Length; i++)
+                        {
+                            if (vals[i] == "") ;
+                            else
+                            {
+                                distance2 = distance2 + vals[i];
+                            }
+                        };
+                        distance.Add(long.Parse(distance2));
+                    }
+                }
+            }
+            long total = 0;
+            long ph = 0;
+
+            for (int j = 0; j < time[0]; j++)
+            {
+                ph = j * (time[0] - j);
+                if (ph > distance[0])
+                {
+                    total++;
+                }
+            }
+
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
